ba60a27edb27730eeb966801a5e0ad4c2bf318f7
Stephan Puget <s.puget38@gmail.com>
2025-02-26 19:13:13 +0100

    Gradio layout improvement

--- src/gradio_interface.py
+++ src/gradio_interface.py
@@ -6,17 +6,15 @@ def create_gradio_interface():
     with gr.Blocks() as demo:
         gr.Markdown("# 📝 Jupyter Notebook Processor")
         notebook_input = gr.File(label="Upload a Jupyter Notebook", type="filepath")
-        summary_output = gr.Textbox(label="Summary", interactive=False)
-        cleaned_notebook_output = gr.File(label="Cleaned Notebook", file_count="single")
-        modularized_output = gr.File(label="Download Modularized Code", file_count="single")
-
         with gr.Row():
             summarize_button = gr.Button("Summarize")
             clean_button = gr.Button("Clean")
             modularize_button = gr.Button("Modularize")
-
+        summary_output = gr.Textbox(label="Summary", interactive=False)
+        with gr.Row():
+            cleaned_notebook_output = gr.File(label="Cleaned Notebook", file_count="single")
+            modularized_output = gr.File(label="Download Modularized Code", file_count="single")
         summarize_button.click(fn=summarize, inputs=[notebook_input], outputs=[summary_output])
         clean_button.click(fn=clean, inputs=[notebook_input], outputs=[cleaned_notebook_output])
         modularize_button.click(fn=modularize, inputs=[notebook_input], outputs=[modularized_output])
-
     demo.launch()
